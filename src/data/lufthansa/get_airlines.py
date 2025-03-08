e68688a768b5cead4daad8b322c320bbe211a501
Melanie <grosso.melanie@gmail.com>
2025-03-08 16:00:13 +0000

    Codes pour requêter les API Countries, Cities, Airports et Airlines

--- src/data/lufthansa/get_airlines.py
+++ src/data/lufthansa/get_airlines.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import time
 
 # Token d'accès obtenu précédemment
-API_KEY = '6mqewaqv4xb2gcn3u4xhb6hp'
+API_KEY = 'trvzrdj7xkzwyex9uqan75p9'
 
 # URL de l'API "Countries"
 url = "https://api.lufthansa.com/v1/mds-references/airlines"
@@ -13,17 +14,58 @@ headers = {
     'Accept': 'application/json'
 }
 
-# Envoi de la requête GET
-response = requests.get(url, headers=headers)
+airlines = [] # initialisation de la liste airlines
+recordLimit = 100 # nombre de résultats rendus par requête (max=100)
+recordOffset = 0 # initialisation du nombre de résultats skipped lors de la reqûete
+totalRequests=1
 
-# Vérifier si la requête a réussi
-if response.status_code == 200:
-    # Analyse de la réponse JSON
-    airlines_data = response.json()
-    # Écrire dans un fichier JSON
-    with open("/home/ubuntu/DST_Airlines/data/lufthansa/airlines.json", "w", encoding="utf-8") as json_file:
-        json.dump(airlines_data, json_file, indent=4, ensure_ascii=False)
-    print("Les airlines ont été enregistrées dans 'airports.json'.")
-else:
-    # Si la requête échoue, afficher le code d'erreur
-    print(f"Erreur {response.status_code}: {response.text}")
+while True:
+    params = {
+        'limit': recordLimit,
+        'offset': recordOffset
+    }
+    print(totalRequests)
+    response = requests.get(url, headers=headers, params=params)
+
+    # Vérifier si la requête a réussi
+    if response.status_code == 200:
+        print("Code 200")
+        data = response.json()
+        airlines.extend(data["AirlineResource"]['Airlines']['Airline'])  # Ajouter les compagnies aériennes à la liste
+        
+        if len(data["AirlineResource"]['Airlines']['Airline']) < 100:
+            print("Toutes les compagnies aériennes ont été récupérées")
+            break  # Si moins de 100 compagnies aériennes sont retournées, on a récupéré tous les compagnies aériennes
+            recordOffset = recordOffset + 100  # Passer aux 100 résultats suivants
+
+        totalRequests += 1  # Incrémenter le nombre de requêtes
+        recordOffset += 100
+            
+    # Respecter la limite de 6 requêtes par seconde (1 requête toutes les 1 seconde maximum)
+    elif response.status_code == 403 :
+        print("Limite seconde atteinte, en pause pendant 1 seconde...")
+        time.sleep(1)  # Attendre 1 seconde après 6 requêtes
+
+    # Respecter la limite de 1000 requêtes par heure
+    elif totalRequests >= 1000:
+        print("Limite horaire atteinte, en pause pendant 1 heure...")
+        time.sleep(3600)  # Attendre 1 heure (3600 secondes)
+        totalRequests = 0  # Réinitialiser le compteur de requêtes après la pause d'une heure
+    
+    # Si problème au niveau de la requête
+    elif response.status_code == 404:
+        print("Code 404")
+        totalRequests += 1  # Incrémenter le nombre de requêtes
+        recordOffset += 1
+
+    else:
+        print(f"Erreur {response.status_code}: {response.text}")
+        break
+        
+        
+        
+with open("/home/ubuntu/DST_Airlines/data/lufthansa/airlines.json", "w", encoding="utf-8") as json_file:
+    json.dump(airlines, json_file, indent=4, ensure_ascii=False)
+print(f"Nombre total de compagnies aériennes récupérées : {len(airlines)}")
+print("Les ompagnies aériennes ont été enregistrées dans 'airlines.json'.")
+    
--- src/data/lufthansa/get_countries.py
+++ src/data/lufthansa/get_countries.py
@@ -1,10 +1,10 @@
 import requests
 import json
+import time
 
 # Token d'accès obtenu précédemment
-API_KEY = '6mqewaqv4xb2gcn3u4xhb6hp'
+API_KEY = 'wa6t9sd254b7cny79ta9ssb8'
 
-# URL de l'API "Countries"
 url = "https://api.lufthansa.com/v1/mds-references/countries"
 
 # En-têtes de la requête, incluant l'authentification
@@ -13,17 +13,51 @@ headers = {
     'Accept': 'application/json'
 }
 
-# Envoi de la requête GET
-response = requests.get(url, headers=headers)
 
-# Vérifier si la requête a réussi
-if response.status_code == 200:
-    # Analyse de la réponse JSON
-    countries_data = response.json()
-    # Écrire dans un fichier JSON
-    with open("/home/ubuntu/DST_Airlines/data/lufthansa/countries.json", "w", encoding="utf-8") as json_file:
-        json.dump(countries_data, json_file, indent=4, ensure_ascii=False)
-    print("Les countries ont été enregistrés dans 'countries.json'.")
-else:
-    # Si la requête échoue, afficher le code d'erreur
-    print(f"Erreur {response.status_code}: {response.text}")
+countries = [] # initialisation de la liste countries
+recordLimit = 100 # nombre de résultats rendus par requête (max=100)
+recordOffset = 0 # initialisation du nombre de résultats skipped lors de la reqûete
+totalRequests=1
+
+while True:
+    params = {
+        'limit': recordLimit,
+        'offset': recordOffset
+    }
+    response = requests.get(url, headers=headers, params=params)
+
+    # Vérifier si la requête a réussi
+    if response.status_code == 200:
+        print(totalRequests)
+        data = response.json()
+        countries.extend(data["CountryResource"]['Countries']['Country'])  # Ajouter les pays à la liste
+        
+        if len(data["CountryResource"]['Countries']['Country']) < 100:
+            print("Tous les pays ont été récupérés")
+            break  # Si moins de 100 pays sont retournés, on a récupéré tous les pays
+            recordOffset = recordOffset + 100  # Passer aux 100 résultats suivants
+
+        totalRequests += 1  # Incrémenter le nombre de requêtes
+        recordOffset += 100
+            
+    # Respecter la limite de 6 requêtes par seconde (1 requête toutes les 1 seconde maximum)
+    elif response.status_code == 403 and response.text == '{ "Error": "Account Over Queries Per Second Limit" }':
+        print("Limite seconde atteinte, en pause pendant 1 seconde...")
+        time.sleep(1)  # Attendre 1 seconde après 6 requêtes
+
+    # Respecter la limite de 1000 requêtes par heure
+    elif totalRequests >= 1000:
+        print("Limite horaire atteinte, en pause pendant 1 heure...")
+        time.sleep(3600)  # Attendre 1 heure (3600 secondes)
+        totalRequests = 0  # Réinitialiser le compteur de requêtes après la pause d'une heure
+    else:
+        print(f"Erreur {response.status_code}: {response.text}")
+        break
+        
+        
+        
+with open("/home/ubuntu/DST_Airlines/data/lufthansa/countries.json", "w", encoding="utf-8") as json_file:
+    json.dump(countries, json_file, indent=4, ensure_ascii=False)
+print(f"Nombre total de pays récupérés : {len(countries)}")
+print("Les countries ont été enregistrés dans 'countries.json'.")
+    
