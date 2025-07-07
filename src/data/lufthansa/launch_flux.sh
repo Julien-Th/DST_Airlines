bf19d31a425571fa5dade20cc09cf97a67b637ff
Ubuntu <ubuntu@ip-172-31-29-64.eu-west-1.compute.internal>
2025-07-07 17:16:55 +0000

    token automatic for all + shells

--- src/data/lufthansa/launch_flux.sh
+++ src/data/lufthansa/launch_flux.sh
@@ -5,10 +5,10 @@ date=$(date '+%Y-%m-%d')
 
 # generate token
 python3 src/data/lufthansa/get_lufthansa_token.py
-# create json flight status of the day
+# create json + csv flight status of the day
 python3 src/data/lufthansa/flux/get_flight_status.py $date
 
 # generate token
 python3 src/data/lufthansa/get_lufthansa_token.py
-# create json multiple flights of the day
+# create json + csv multiple flights of the day
 python3 src/data/lufthansa/flux/get_multiple_flights.py $date
