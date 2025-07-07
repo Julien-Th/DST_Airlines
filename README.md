d39097bf4caea569540a7bd699f60eaaae858f05
Ubuntu <ubuntu@ip-172-31-29-64.eu-west-1.compute.internal>
2025-07-07 17:02:48 +0000

    token automatic for all + shells

--- README.md
+++ README.md
@@ -11,11 +11,20 @@ https://developer.lufthansa.com/io-docs#/
 Steps to get passenger flights data :
 - register at https://developer.lufthansa.com/
 - follow the instructions until you get your **Key** and your **Secret** (after login)
-- execute ```
-    python3 src/data/lufthansa/get_lufthansa_token.py
-```
-- copy your token and replace it in `src/data/lufthansa/get_lufthansa_data.py` on line 4
-- execute ```
-    python3 get_flight_schedules.py
-```
-- see the result in `data/lufthansa/horaires_vols.json`
+
+#### Execute one script
+
+- execute `python3 src/data/lufthansa/get_lufthansa_token.py`
+
+- execute your script with `python3 + name of script`
+
+- see the result in `data/lufthansa/`
+
+
+#### Get all referentiels
+
+- execute `sh src/data/lufthansa/launch_referentiels.sh`
+
+#### Get flux
+
+- execute `sh src/data/lufthansa/launch_flux.sh`
--- src/data/lufthansa/referentiels/get_cities.py
+++ src/data/lufthansa/referentiels/get_cities.py
@@ -1,20 +1,27 @@
 import requests
 import json
 import time
+import os
 
-# Token d'accès obtenu précédemment
-API_KEY = 'hme5hjzcgerkycamj4sj3aug'
+# 🔹 Charger le token depuis un fichier
+token_path = "/home/ubuntu/DST_Airlines/data/token/access_token.txt"
+if not os.path.exists(token_path):
+    print(f"❌ Token introuvable à : {token_path}")
+    sys.exit(1)
+
+with open(token_path, "r") as f:
+    access_token = f.read().strip()
 
 # URL de l'API "Countries"
-url = "https://api.lufthansa.com/v1/mds-references/airlines"
+url = "https://api.lufthansa.com/v1/mds-references/cities"
 
 # En-têtes de la requête, incluant l'authentification
 headers = {
-    'Authorization': f'Bearer {API_KEY}',
+    'Authorization': f'Bearer {access_token}',
     'Accept': 'application/json'
 }
 
-airlines = [] # initialisation de la liste airlines
+cities = [] # initialisation de la liste cities
 recordLimit = 100 # nombre de résultats rendus par requête (max=100)
 recordOffset = 0 # initialisation du nombre de résultats skipped lors de la reqûete
 totalRequests=1
@@ -24,25 +31,24 @@ while True:
         'limit': recordLimit,
         'offset': recordOffset
     }
-    print(totalRequests)
     response = requests.get(url, headers=headers, params=params)
 
     # Vérifier si la requête a réussi
     if response.status_code == 200:
-        print("Code 200")
+        print(totalRequests)
         data = response.json()
-        airlines.extend(data["AirlineResource"]['Airlines']['Airline'])  # Ajouter les compagnies aériennes à la liste
+        cities.extend(data["CityResource"]['Cities']['City'])  # Ajouter les villes à la liste
         
-        if len(data["AirlineResource"]['Airlines']['Airline']) < 100:
-            print("Toutes les compagnies aériennes ont été récupérées")
-            break  # Si moins de 100 compagnies aériennes sont retournées, on a récupéré tous les compagnies aériennes
+        if len(data["CityResource"]['Cities']['City']) < 100:
+            print("Toutes les villes ont été récupérées")
+            break  # Si moins de 100 villes sont retournées, on a récupéré toutes les villes
             recordOffset = recordOffset + 100  # Passer aux 100 résultats suivants
 
         totalRequests += 1  # Incrémenter le nombre de requêtes
         recordOffset += 100
             
     # Respecter la limite de 6 requêtes par seconde (1 requête toutes les 1 seconde maximum)
-    elif response.status_code == 403 :
+    elif response.status_code == 403 and response.text == '{ "Error": "Account Over Queries Per Second Limit" }':
         print("Limite seconde atteinte, en pause pendant 1 seconde...")
         time.sleep(1)  # Attendre 1 seconde après 6 requêtes
 
@@ -51,21 +57,14 @@ while True:
         print("Limite horaire atteinte, en pause pendant 1 heure...")
         time.sleep(3600)  # Attendre 1 heure (3600 secondes)
         totalRequests = 0  # Réinitialiser le compteur de requêtes après la pause d'une heure
-    
-    # Si problème au niveau de la requête
-    elif response.status_code == 404:
-        print("Code 404")
-        totalRequests += 1  # Incrémenter le nombre de requêtes
-        recordOffset += 1
-
     else:
         print(f"Erreur {response.status_code}: {response.text}")
         break
         
         
         
-with open("/home/ubuntu/DST_Airlines/data/lufthansa/airlines.json", "w", encoding="utf-8") as json_file:
-    json.dump(airlines, json_file, indent=4, ensure_ascii=False)
-print(f"Nombre total de compagnies aériennes récupérées : {len(airlines)}")
-print("Les compagnies aériennes ont été enregistrées dans 'airlines.json'.")
+with open("/home/ubuntu/DST_Airlines/data/lufthansa/cities.json", "w", encoding="utf-8") as json_file:
+    json.dump(cities, json_file, indent=4, ensure_ascii=False)
+print(f"Nombre total de villes récupérées : {len(cities)}")
+print("Les villes ont été enregistrées dans 'cities.json'.")
     
